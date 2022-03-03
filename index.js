0e14cdb316d161f2554871d88c7d18ceb4d848e8
Ross <rmcwey@hotmail.com>
2022-03-03 06:07:17 -0500

    added files through npm init and i inquirer. Added some prompts.

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    {
+        type: 'input',
+        message: "What would you like your title to be named?",
+        name: "title",
+    },
+    {
+        type: 'input',
+        message: "Please write a breif description of your project",
+        name: "description",
+    },
+    {
+        type: 'input',
+        message: "Please select a license",
+        name: "license",
+    },
+    {
+        type: 'input',
+        message: "Please input your Github User Name",
+        name: "github",
+    },
+    {
+        type: 'input',
+        message: "Please input your contact email",
+        name: "email",
+    },
+]).then((answers) => {
+    console.log(answers)
+});
+// const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
