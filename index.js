e1f73a98f1696c8396ced64db745da73e9c1e564
Ross <rmcwey@hotmail.com>
2022-03-04 01:39:06 -0500

    added readme text

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ inquirer.prompt([
     },
     {
         type: 'input',
+        message: "Please input the site URL",
+        name: "url",
+    },
+    {
+        type: 'input',
         message: "Please select a license",
         name: "license",
     },
@@ -31,6 +36,48 @@ inquirer.prompt([
 ]).then((answers) => {
     console.log(answers)
 });
+
+const generateREADME = ({title, description, url, license, github, email}) => {
+    return `# ${title}
+    ### Table of contents
+    * [Description](#description)
+    * [Built with](#built-with)
+    * [Screen Shot](#screen-shot)
+    * [Application URL](#application-url)
+    * [License](#license)
+    
+    
+    ### Description: 
+    ${description}
+    <br>
+    
+    ### Built with: 
+    This application is built using :
+    * HTML
+    * CSS
+    * Bootstrap
+    * JavaScript
+    
+    ### Screen Shot:
+    
+    ### Application URL:
+    
+    ${url}
+
+    <br>
+
+    ### Github
+    github.com/${github}
+
+    ### Questions
+    For any questions please contact me at ${email}
+
+    ### License: ${license}
+    
+    `;
+}
+fs.writeFile('README.md', generateREADME(answers), (err) =>
+    err ? console.error(err) : console.log('Commit logged!'));
 // const questions = [];
 
 // TODO: Create a function to write README file
